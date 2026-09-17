15d9b1853c427fa5d04f1a9f60e0956f19c178f3
Tom Clemmet <tomclemmet@gmail.com>
2026-09-17 12:26:30 +0100

    Analyse baseline characteristics

---
+++ R/meta-analyse/baseline-chars.R
@@ -0,0 +1,30 @@
+rm(list = ls())
+library(dplyr)
+library(ggplot2)
+theme_set(theme_bw())
+
+con <- dbConnect(RSQLite::SQLite(), "app/psoriasis-rcts.sqlite")
+chars <- dbReadTable(con, "v_baseline")
+
+outcomes <- c(
+  "Age", "Sex (n male)", "Ethnicity (n white)", "Weight", "BMI", 
+  "Duration of psoriasis", "PASI", "Psoriatic arthritis", 
+  "Systemic agent", "Biologic agents"
+)
+
+chars |> 
+  select(ref_id, trial, drug, dose, characteristic, data_type, k, n, mean, sd) |> 
+  mutate(mean = if_else(is.na(mean) & !is.na(k), k / n, mean),
+         data_type = if_else(characteristic %in% c("PASI", "Duration of psoriasis"), "Psoriasis", data_type)) |> 
+  filter(characteristic %in% c(
+    "Age", "Sex (n male)", "Ethnicity (n white)", "Weight", "BMI", 
+    "Duration of psoriasis", "PASI", "Psoriatic arthritis", 
+    "Systemic agent", "Biologic agents"
+  )) |> 
+  ggplot() +
+  geom_boxplot(aes(x = mean, y = characteristic, fill = characteristic), show.legend = FALSE) +
+  facet_wrap(~ data_type, scales = "free", space = "free_y", nrow = 3)
+
+chars |> 
+  filter(characteristic == "Psoriatic arthritis") |> mutate(p = k/n) |> 
+  View()
